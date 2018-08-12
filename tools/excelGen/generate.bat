f60463f0b739f3d111d87aa7522864fcf093602f
yijunjian <yijunjian@vip.qq.com>
2018-08-12 14:29:43 +0800

    1.1

--- tools/excelGen/generate.bat
+++ tools/excelGen/generate.bat
@@ -7,9 +7,7 @@ del /s /q .\py\*.*
 
 python tool.py BUILDING xls\Core_Sys.xlsm
 python tool.py ITEM_RES xls\Core_Sys.xlsm
-python tool.py BLDG_FUNC_PARAM xls\Core_Sys.xlsm
 
-python tool.py DaMi xls\Bldg_Func.xlsm
 
 protoc.exe --java_out=./java proto/*.proto
 
